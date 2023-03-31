6c4aca6e34ae6f8843fc682b5a77be9fa2215882
Dan Coates <dcoates72@gmail.com>
2023-03-30 17:50:23 -0700

    Add ffmpeg files to dist.

--- make.bat
+++ make.bat
@@ -1,1 +1,9 @@
 pyinstaller --noconfirm -D SabView.py
+
+rem YUCK! Need to copy in ffmpeg stuff
+rem TODO: Get appropriate directory from Windows path "where" etc
+copy c:\users\vimal\anaconda3\envs\torsion\Library\bin\ff*.exe dist\SabView
+copy c:\users\vimal\anaconda3\envs\torsion\Library\bin\av*.dll dist\SabView
+copy c:\users\vimal\anaconda3\envs\torsion\Library\bin\postproc-55.dll  dist\SabView
+copy c:\users\vimal\anaconda3\envs\torsion\Library\bin\swscale-5.dll dist\SabView
+copy c:\users\vimal\anaconda3\envs\torsion\Library\bin\swresample-3.dll dist\SabView
